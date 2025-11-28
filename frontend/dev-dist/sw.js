e44b12ca5e4ae5e2c5ca7ea74026196bfc8df38a
Sahadev Neupane <sahadevneupane520@gmail.com>
2025-11-28 09:21:22 +0100

    Add project screenshots to README

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cq4n1dh44fk"
+    "revision": "0.q08ltohd574"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
